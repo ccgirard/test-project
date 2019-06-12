ba5744ea6d0f0a44437bf4377e78262027c33026
Chris Girard <chris@routifc.com>
2019-06-12 13:46:56 -0700

    using existing antdesign button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,51 @@
-import React from "react";
+import * as React from "react";
 import { Button } from "antd";
-
 import 'antd/dist/antd.css';
 
-const ButtonPrimary = (props) =>{
-    return(
-        <Button type="danger">{props.text}</Button>
-    )
-}
+export default class AntButton extends React.Component {
+  // Set default properties
+  static defaultProps = {
+    width: 68,
+    height: 32,
+    label: "Label",
+    type: "default",
+    size: "default",
+    icon: "",
+    disabled: false,
+    ghost: false,
+    circle: false,
+    onClick: () => {}
+  };
 
-export default ButtonPrimary;
+  render() {
+    const { label, type, size, icon, disabled, ghost, circle, onClick } = {
+      ...this.props
+    };
+    const labeledButton = (
+      <Button
+        type={type}
+        size={size}
+        icon={icon}
+        disabled={disabled}
+        ghost={ghost}
+        onClick={onClick}
+        block={true}
+      >
+        {label}
+      </Button>
+    );
+    const circledButton = (
+      <Button
+        type={type}
+        size={size}
+        icon={icon}
+        disabled={disabled}
+        ghost={ghost}
+        onClick={onClick}
+        shape="circle"
+      />
+    );
+
+    return circle ? circledButton : labeledButton;
+  }
+}

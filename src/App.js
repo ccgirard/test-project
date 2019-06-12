7bae6cf39f471f234144f0979ca6f927bfb0747e
Chris Girard <chris@routifc.com>
2019-06-12 13:11:45 -0700

    adding ant button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Header from "./component/Header"
+import Header from "./components/Header"
+import Button from "./components/Button"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Button/>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+
+import 'antd/dist/antd.css';
+
+const ButtonPrimary = () =>{
+    return(
+        <Button type="primary">Button</Button>
+    )
+}
+
+export default ButtonPrimary;
